4726ad49058b818715b0557a6732d56b90d55bc7
ttochino <sujeong0908@gmail.com>
2022-11-09 17:39:26 +0900

    1109

---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div style={{ display: "flex", width: "100%", height: "100vh", justifyContent: "center", alignItems: "center" }}>
+      <button>button</button>
+    </div>
+  );
+}
